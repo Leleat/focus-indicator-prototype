a1caea27222a95841f2415d7da2605d2bc07c0d9
Leleat <atule@pm.me>
2023-02-28 14:44:23 +0100

    Remove loading spinner since it's its own extension now

--- extension.js
+++ extension.js
@@ -30,7 +30,6 @@ const { CustomAppSwitcher } = Me.imports.src.altTab;
 const { CustomWorkspaceAnimation } = Me.imports.src.workspaceAnimation;
 const { CustomSwitchToApplication } = Me.imports.src.switchToApplication;
 const { IdleIndicator } = Me.imports.src.idleIndicator;
-const { LoadingSpinner } = Me.imports.src.loadingSpinner;
 
 class Extension {
     constructor() {
@@ -43,15 +42,11 @@ class Extension {
         this._customSwitchToApplication = new CustomSwitchToApplication();
         this._customSwitchToApplication = new CustomSwitchToApplication();
         this._idleIndicator = new IdleIndicator();
-        this._loadingSpinner = null;
 
         this._settings = ExtensionUtils.getSettings(Me.metadata['settings-schema']);
 
         this._settings.connect('changed::hide-app-menu', () => this._setAppMenu());
         this._setAppMenu();
-
-        this._settings.connect('changed::loading-animation', () => this._setLoadingSpinner());
-        this._setLoadingSpinner();
 
         if (this._wasLocked) {
             this._unlockId = Main.screenShield.actor.connect('hide', () => {
@@ -88,9 +83,6 @@ class Extension {
         this._focusIndicator.destroy();
         this._focusIndicator = null;
 
-        this._loadingSpinner?.destroy();
-        this._loadingSpinner = null;
-
         // Looks like g-s updates the top panel after the extensions are disabled
         // so we want to exit early here otherwise the appMenu will be shown on
         // the lockscreen while the extension is enabled...
@@ -107,14 +99,6 @@ class Extension {
             Main.panel.statusArea['appMenu'].container.hide();
         else
             Main.panel.statusArea['appMenu'].container.show();
-    }
-
-    _setLoadingSpinner() {
-        this._loadingSpinner?.destroy();
-        this._loadingSpinner = null;
-
-        if (this._settings.get_boolean('loading-animation'))
-            this._loadingSpinner = new LoadingSpinner();
     }
 }
 
